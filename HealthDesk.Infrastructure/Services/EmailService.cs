6c2f6dad22e686cfc95695f661166aeaad845d6a
Aysenur Mengi <aaysenurmengi@gmail.com>
2025-11-24 18:11:27 +0300

    (feat) : update services/emailservice

--- HealthDesk.Infrastructure/Services/EmailService.cs
+++ HealthDesk.Infrastructure/Services/EmailService.cs
@@ -1,13 +1,43 @@
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
 using HealthDesk.Application.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace HealthDesk.Infrastructure.Services
 {
     public class EmailService : IEmailService
     {
-        public Task SendEmailAsync(string to, string subject, string body, CancellationToken cancellationToken = default)
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
         {
-             Console.WriteLine($"E-posta gönderildi: To={to}, Subject={subject}, Body={body}");
-            return Task.CompletedTask;
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body, CancellationToken cancellationToken = default)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+
+            var smtpClient = new SmtpClient(emailSettings["Host"])
+            {
+                Port = int.Parse(emailSettings["Port"]!),
+                Credentials = new NetworkCredential(emailSettings["UserName"], emailSettings["Password"]),
+                EnableSsl = bool.Parse(emailSettings["EnableSSL"]!)
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(emailSettings["UserName"]!, "HealthDesk Notifications"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(to);
+
+            await smtpClient.SendMailAsync(mailMessage, cancellationToken);
         }
     }
 }
